65d7cd22151559310e78b8d69d502dfecab21c1b
MaheshAgga <maheshagga8@gmail.com>
2024-04-12 13:28:00 +0530

    CD

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -27,6 +27,6 @@ variable "app_version" {
   
 }
 
-# variable "iam_instance_profile" {
-#   default = "ShellScriptRoleForRoboshop"
-# }
+variable "iam_instance_profile" {
+  default = "ShellScriptRoleForRoboshop"
+}
